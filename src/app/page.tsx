f829560dbaa347ae448706ff8336af81cd323d47
tohkailiang <tohkailiang@gmail.com>
2025-05-30 21:28:01 +0800

    Chose font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import { Button } from "@/components/ui/button";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/mode-toggle";
 import NameCard from "@/components/name-card";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex flex-row items-center justify-center min-h-svh gap-4">
+      <div className="flex flex-row items-center justify-center min-h-svh">
         <NameCard />
-        <Button>Click me</Button>
-        <ModeToggle />
       </div>
     </ThemeProvider>
   );
--- src/components/name-card.tsx
+++ src/components/name-card.tsx
@@ -1,10 +1,21 @@
 import { Card, CardTitle, CardDescription } from "./ui/card";
+import { DM_Sans } from "next/font/google";
+
+const font = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700", "800"],
+});
 
 function NameCard() {
   return (
-    <Card className="w-2xs p-8">
-      <CardTitle className="text-2xl text">Toh Kai Liang</CardTitle>
-      <CardDescription>Software Developer</CardDescription>
+    <Card className="w-2xl px-10 py-8 gap-0">
+      <CardTitle className={`${font.className} text-5xl`}>Kai Liang</CardTitle>
+      <CardDescription className={`${font.className} text-xl`}>
+        Software Developer
+      </CardDescription>
+      <CardDescription className={`${font.className}`}>
+        Singapore
+      </CardDescription>
     </Card>
   );
 }
