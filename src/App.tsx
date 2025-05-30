fb8427b002d7cd45d8f9e32d9b16d893aa81ccff
tohkailiang <tohkailiang@gmail.com>
2025-05-30 18:30:15 +0800

    Rombo setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { ThemeProvider } from "./components/theme-provider";
 import { ModeToggle } from "./components/mode-toggle";
+import NameCard from "./components/NameCard";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="flex flex-row items-center justify-center min-h-svh gap-4">
+        <NameCard />
         <Button>Click me</Button>
         <ModeToggle />
       </div>
---
+++ src/components/NameCard.tsx
@@ -0,0 +1,9 @@
+function NameCard() {
+  return (
+    <div className="motion-opacity-in-0 motion-translate-y-in-100 motion-blur-in-md motion-duration-500">
+      Toh Kai Liang
+    </div>
+  );
+}
+
+export default NameCard;
